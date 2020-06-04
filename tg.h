d808d2cfabd8fa93399dca58f96aa8aeb43afb2b
Fehmina <54239381+fehmina-hasan@users.noreply.github.com>
2020-06-04 15:24:39 -0700

    trigram header file

---
+++ tg.h
@@ -0,0 +1,25 @@
+#ifndef _lang_h
+#define _lang_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Lang {
+private:
+	int len = 27*27*27;
+
+	//get a getter method
+	int *freq;  //pointer
+	
+	void trigram(string language);
+
+public:
+	Lang(string l); // calls trigram
+	~Lang();
+	int getArray(int i);
+	int getLen();
+};
+
+#endif
