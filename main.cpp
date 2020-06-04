144bd658a64c5bc10cb7a6c895904b37925ebdb2
Fehmina <54239381+fehmina-hasan@users.noreply.github.com>
2020-06-04 15:25:05 -0700

    source files

---
+++ main.cpp
@@ -0,0 +1,84 @@
+ 
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<fstream>
+#include<ostream>
+#include<vector>
+#include "tg.h"
+using namespace std;
+
+//calls distance function
+double distance(Lang A, Lang B);
+
+int main(int argc, char *argv[]) {
+    //exeptional handling
+    if(argc < 3 ){
+        throw runtime_error("Invalid");
+    }
+    
+    if(argc >= 3){
+        double max = 0;
+
+        //calling infile & myfile
+        ifstream myfile;
+        ifstream infile;
+        vector<double> cosines;
+
+        //made two parallel strings(test/training files) and made them into objects which is then compared in the distance function
+        for(int i = 1; i <= (int)argc-2; i++){
+            double dist = 0;
+            string line;
+            myfile.open(argv[i]);
+
+            //exeptional handling
+            if(myfile.fail()){
+                throw runtime_error("ahh");
+            }
+            string body;
+            while(getline(myfile,line)){
+                body+= line;
+            }
+            myfile.close();
+            Lang culture(body);
+            string line2;
+            infile.open(argv[argc-1]);
+
+            //exeptional handling
+            if(infile.fail()){
+                throw runtime_error("ahh");
+            }
+            string body2;
+            while(getline(infile,line2)){
+                body2 += line2;
+            }
+            infile.close();
+            Lang test(body2);
+
+
+            dist = distance(culture, test);
+            cosines.push_back(dist);
+
+        }
+
+        int num = 0;
+        int pos = 0;
+        max = cosines[0];
+
+        //comparing command line arguments and vector of cosines to find the highest cosine value and its relating language postition in the list
+        for(int i = 1; i <= (int)argc-2; i++){
+            for(int j = 0; j < (int)cosines.size()-1; j++){
+                if(max <= cosines[j]){
+                    max = cosines[j];
+                    pos = j + 1;
+                }
+            num = pos;
+            }
+        }
+
+        //printing the most similar language name to the query
+        cout << argv[pos] << std::endl;
+
+    return 0;
+    }
+}
---
+++ tg.cpp
@@ -0,0 +1,74 @@
+// Name: Fehmina Hasan
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "tg.h"
+using namespace std;
+
+//contructor
+Lang::Lang(string language){
+    freq = new int[len]; //variable that initializes the array
+
+    //variable that initializes the array to all 0's
+    for (int i=0; i < len; i++) {
+        freq[i] = 0;
+    }
+    trigram(language);
+}
+
+//destructor
+Lang::~Lang(){
+}
+
+//gets the each element trigram in each language
+int Lang::getArray(int i ){
+    return freq[i];
+}
+//getter function
+int Lang::getLen(){
+    return len;
+}
+
+//void function that adds 1 to the index in an array
+void Lang::trigram(string language){
+
+    int trigrams = 0;
+    for(int i = 0; i < (int)language.length()-2; i++){
+        trigrams = 0;
+        if(language[i] == ' '){
+            trigrams += 0;
+        }
+        else{
+            trigrams += (int)((language[i]-96) * (27*27));
+        }
+        if(language[i+1] == ' '){
+            trigrams += 0;
+        }
+        else{
+            trigrams += (int)((language[i+1]-96) * (27));
+        }
+        if(language[i+2] == ' '){
+            trigrams += 0;
+        }
+        else{
+            trigrams += (int)((language[i+2]-96));
+        }
+        freq[trigrams]++;
+    }
+
+}
+//cosine similarity function computes the value of each language
+double distance(Lang A, Lang B){
+    unsigned long long x = 0, y = 0, z = 0;
+    for(int i = 0; i <A.getLen(); i++){
+        x += A.getArray(i) * B.getArray(i);
+        y += A.getArray(i) * A.getArray(i);
+        z += B.getArray(i) * B.getArray(i);
+    }
+    return (double) x / (sqrt(y) * sqrt(z));
+}
